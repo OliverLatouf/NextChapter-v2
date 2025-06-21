ec0458eb0b8576cf1280a0ce4e3767532e17f5f6
Oliver Latouf <oli-macpro@Olivers-MacBook-Pro.local>
2025-06-21 17:16:58 +0100

    Update Stripe API version to 2025-05-28.basil

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -7,6 +7,6 @@ export const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHAB
 export async function getServerStripe() {
   const Stripe = (await import('stripe')).default
   return new Stripe(process.env.STRIPE_SECRET_KEY!, {
-    apiVersion: '2024-06-20',
-  })
+    apiVersion: '2025-05-28.basil',
+    })
 }
